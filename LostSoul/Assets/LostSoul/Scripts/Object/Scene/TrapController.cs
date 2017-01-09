7c2b7a8fa3fa0ec3f91194f267c6a83fa2cacc86
PhantomGrapes <phantomgrapes@gmail.com>
2017-01-09 22:40:54 +0100

    traps in AnimationMC1

---
+++ LostSoul/Assets/LostSoul/Scripts/Object/Scene/TrapController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapController : MonoBehaviour {
+    private MainCharacter player;
+
+    void Start()
+    {
+        player = FindObjectOfType<MainCharacter>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<MainCharacter>() != null)
+        {
+            player.BeAttacked(player.maxHp + 1);
+        }
+    }
+}
