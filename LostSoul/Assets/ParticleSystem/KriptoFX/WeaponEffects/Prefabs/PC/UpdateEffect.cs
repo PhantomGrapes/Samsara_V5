45d9b972d0356ec128888cdf3b975100c688e458
Gregory24 <zsl_24@163.com>
2017-01-09 20:58:45 +0100

    test meshsprite particl

---
+++ LostSoul/Assets/ParticleSystem/KriptoFX/WeaponEffects/Prefabs/PC/UpdateEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateEffect : MonoBehaviour {
+
+    public GameObject myMesh;
+
+	void Start () {
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var sh = ps.shape;
+        sh.enabled = true;
+        sh.shapeType = ParticleSystemShapeType.Mesh;
+        sh.mesh = myMesh;
+	}
+
+}
