9fa3326bf1724bb553036683895223f715166e5a
PhantomGrapes <phantomgrapes@gmail.com>
2017-01-10 15:36:45 +0100

    move trap prefab to artwork

---
+++ LostSoul/Assets/LostSoul/Scripts/Object/Scene/CoolDownController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class CoolDown
+{
+    public string name;
+    public float coolDownLength;
+    public float currentCoolDown;
+    public Image icon;
+}
+
+public class CoolDownController : MonoBehaviour {
+
+    public List<CoolDown> coolDowns;
+	// Use this for initialization
+	
+
+	// Update is called once per frame
+	void Update () {
+	    foreach(CoolDown c in coolDowns)
+        {
+            if (c.currentCoolDown < c.coolDownLength)
+            {
+                c.currentCoolDown += Time.deltaTime;
+                c.icon.fillAmount = c.currentCoolDown / c.coolDownLength;
+            }
+        }
+	}
+}
