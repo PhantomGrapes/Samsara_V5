35bd9029b6e7304ef76f90458d29690cdead3da6
xtk923 <xiongtk@gmail.com>
2017-01-10 13:26:54 +0100

    working on Lei

---
+++ LostSoul/Assets/LostSoul/Scripts/Object/Livings/Monster/Boss/Boss2/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float speed;
+	public Vector2 direction;
+	MainCharacter target;
+	Rigidbody2D rigi;
+	bool initialVelocitySet = false;
+	public float duration;
+	// Use this for initialization
+	void Start () {
+		target = FindObjectOfType<MainCharacter> ();
+		rigi = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void FixedUpdate () {
+		if (!initialVelocitySet) {
+			rigi.velocity = GetVelocity ();
+		}
+		print (rigi.velocity);
+	
+	}
+
+	public Vector2 GetVelocity(){
+		initialVelocitySet = true;
+		return (target.transform.position - this.transform.position).normalized * speed;
+	}
+
+	public IEnumerator DestroyProjectile(){
+		yield return new WaitForSeconds (duration);
+		this.gameObject.SetActive (false);
+	}
+}
