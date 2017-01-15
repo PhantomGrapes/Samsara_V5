a9ee02e26e2b36324cc90329374b3b16dd93f993
xtk923 <xiongtk@gmail.com>
2017-01-15 18:09:02 +0100

    implemented projectiles and mirrors for Lei. Projectiles will be reflected upon hitting a mirror:

--- LostSoul/Assets/LostSoul/Scripts/Object/Livings/Monster/Boss/Boss2/Mirror.cs
+++ LostSoul/Assets/LostSoul/Scripts/Object/Livings/Monster/Boss/Boss2/Mirror.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class Mirror : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
-	
+
+		
 	}
 	
 	// Update is called once per frame
@@ -13,12 +15,5 @@ public class Mirror : MonoBehaviour {
 	
 	}
 
-//	// update the normal/perpendicular vector of the mirror
-//	void GetNormal(){
-//		BoxCollider2D body = GetComponent<BoxCollider2D> ();
-//		if (body.size.x >= body.size.y) {
-//			
-//		}
-//	}
 
 }
