9f04631d5ac57592730b3316921a6b6b5202850b
xtk923 <xiongtk@gmail.com>
2017-05-04 15:15:26 +0200

    camara size changed

--- LostSoul/Assets/LostSoul/Scripts/Object/Scene/ForeGroundFollowCamera.cs
+++ LostSoul/Assets/LostSoul/Scripts/Object/Scene/ForeGroundFollowCamera.cs
@@ -8,7 +8,7 @@ public class ForeGroundFollowCamera : MonoBehaviour {
 	
 	void FixedUpdate () {
 		transform.position = target.transform.position + new Vector3 (0f, 0f, 10f);
-		scale = (target.GetComponent<Camera> ().orthographicSize / target.GetComponent<CameraFollow_revised> ().defaultRatio)* 1.05f;
+		scale = (target.GetComponent<CameraFollow_revised> ().zoomRatio)/target.GetComponent<Camera> ().orthographicSize*25f;
 		transform.localScale = new Vector3 (scale, scale, scale);
 	}
 }
